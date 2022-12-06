65d64ab8619e679f9da38db7afe96787a0397f02
karimy <karim.alrashdan14@gmail.com>
2022-12-05 18:26:26 -0700

    Create Sleep data sample files

---
+++ src/data/sample-weekSleepData.js
@@ -0,0 +1,11 @@
+const userWeekSleepData = [
+  {userID: 1, date: '2019/06/15', hoursSlept: 6.1, sleepQuality: 2.2},
+  {userID: 1, date: "2019/06/16", hoursSlept: 4.1, sleepQuality: 3.8},
+  {userID: 1, date: "2019/06/18", hoursSlept: 10.4, sleepQuality: 3.1},
+  {userID: 1, date: "2019/06/19", hoursSlept: 10.7, sleepQuality: 1.2},
+  {userID: 1, date: "2019/06/20", hoursSlept: 9.3, sleepQuality: 1.2},
+  {userID: 1, date: "2019/06/21", hoursSlept: 7.8, sleepQuality: 4.2},
+  {userID: 1, date: '2019/06/22', hoursSlept: 10.8, sleepQuality: 4.7},
+  {userID: 1, date: '2019/06/23', hoursSlept: 10.5, sleepQuality: 3.7}]
+
+  module.exports= userWeekSleepData;
---
+++ test/Sleep-Test.js
@@ -0,0 +1,11 @@
+import { expect } from 'chai'
+import Sleep from '../src/Sleep-Class'
+const sleepData = require('../src/data/sample-data')
+const weekSleepData = require('../src/data/sample-weekSleepData')
+
+
+
+describe('Sleep', () => {
+ console.log('user sleep',sleepData)
+  console.log('week sleep', weekSleepData)
+})

687f748241e7834050b0eea5d9dcc7863df9652b
Karrar Qasim <Karawr1996@gmail.com>
2022-12-06 19:23:58 -0700

    Added more tests to sleep class

--- test/Sleep-Test.js
+++ test/Sleep-Test.js
@@ -23,19 +23,23 @@ it('should keep track of sleep data', function() {
   expect(sleep.sleepData).to.eql(sleepDataSample) //change here
 })
 
+it('should return all sleep info for one user', function() {
+  expect(sleep.filterSleepByUser(sleep.sleepData[0].userID)).to.eql([{userID: 1, date: '2019/06/15', hoursSlept: 6.1, sleepQuality: 2.2}])
+})
+
 it('should calculate the average hours of sleep per day', function(){
   expect(sleep.calculateAverageSleep('hoursSlept')).to.equal(9)
-}) 
+})
 
 it('should calculate the average sleep quality per day', function() {
-  expect(sleep.calculateAverageSleep('sleepQuality')).to.equal(4)
+  expect(sleep.calculateAverageSleep('sleepQuality',)).to.equal(4)
 })
 
 it('should calculate sleep hours per day by date', function() {
-  expect(sleep.calculateSleepPerDay('2019/06/17', 'hoursSlept')).to.equal(10.5)
+  expect(sleep.calculateSleepPerDay('2019/06/15', 'hoursSlept', 1)).to.eql([6.1])
 })
 
 it('should calculate sleep quality per day by date', function() {
-  expect(sleep.calculateSleepPerDay('2019/06/17', 'sleepQuality')).to.equal(3.7)
+  expect(sleep.calculateSleepPerDay('2019/06/15', 'sleepQuality', 1)).to.eql([2.2])
 })
 })

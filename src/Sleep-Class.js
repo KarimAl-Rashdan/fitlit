cdda1af6887d67662a8b33b9838da0edfbbf7c78
Karrar Qasim <Karawr1996@gmail.com>
2022-12-06 19:25:13 -0700

    Added filter id method to sleep class

--- src/Sleep-Class.js
+++ src/Sleep-Class.js
@@ -1,16 +1,23 @@
 class Sleep {
   constructor(userSleepData) {
-    console.log(userSleepData)
     this.sleepData = userSleepData;
-
+  }
+  filterSleepByUser(id) {
+    let totalUserData = this.sleepData.filter(user => user.userID === id)
+    return totalUserData
   }
   calculateAverageSleep(type) {
-    console.log('this.sleepData', this.sleepData)
     let total = this.sleepData.reduce((total, num) => {
       return total += num[type]
     }, 0)
     return Math.round(total/this.sleepData.length)
   }
+  calculateSleepPerDay(date, type, id) {
+    let value = this.filterSleepByUser(id)
+    let dataByDate = value
+    .filter(user => user.date === date)
+    .map(user => user[type])
+    return dataByDate
+  }
 }
-
 export default Sleep;

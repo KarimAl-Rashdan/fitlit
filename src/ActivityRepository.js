5fe2d46a4af03578ee4f302163ccf869b6316161
karimy <karim.alrashdan14@gmail.com>
2023-01-04 13:47:56 -0700

    Create filterById function

--- src/ActivityRepository.js
+++ src/ActivityRepository.js
@@ -1,6 +1,11 @@
 class ActivityRepository {
-  constructor() {
-
+  constructor(allActivityData) {
+    this.activityData = allActivityData;
+    this.currentUsersActivities = null;
+  }
+  filterById(currentUserId) {
+    const usersActivities = this.activityData.filter(activity => activity.userID === currentUserId)
+    return usersActivities
   }
 }
 

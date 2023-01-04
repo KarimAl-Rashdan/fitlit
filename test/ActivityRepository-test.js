2cfecff9310158e2c93efa639c173bd99a16b55f
karimy <karim.alrashdan14@gmail.com>
2023-01-03 20:33:10 -0700

    Fix import file names

--- test/ActivityRepository-test.js
+++ test/ActivityRepository-test.js
@@ -1,7 +1,37 @@
-import { expect } from chai
+import { expect } from "chai"
 import sampleActivityData from "../src/data/sample-acitvity"
 import ActivityRepository from "../src/ActivityRepository"
 import User from "../src/User"
 
 
 
+describe('ActivityRepository', () => {
+  let activityData;
+  let activity1;
+  let user1;
+
+  beforeEach(() => {
+    activityData = sampleActivityData
+    activity1 = new ActivityRepository(activityData)
+    user1 = new User({
+      "id": 1,
+      "name": "Luisa Hane",
+      "address": "15195 Nakia Tunnel, Erdmanport VA 19901-1697",
+      "email": "Diana.Hayes1@hotmail.com",
+      "strideLength": 4.3,
+      "dailyStepGoal": 10000,
+      "friends": [
+        16,
+        4,
+        8
+      ]
+    })
+  })
+  it("should be a function", function() {
+    expect(ActivityRepository).to.be.a("function")
+  })
+
+
+
+
+})
